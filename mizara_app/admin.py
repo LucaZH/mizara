4441d05d7718b04b556c3bad212a843e33ad7e94
LucaZH <lucarandria1@gmail.com>
2023-04-25 18:32:28 +0300

    update model part

--- mizara_app/admin.py
+++ mizara_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Fichier)
-admin.site.register(HistoriqueTransfert)
-admin.site.register(Transfert)
+admin.site.register(File)
+admin.site.register(TransferHistory)
+admin.site.register(Transfer)
